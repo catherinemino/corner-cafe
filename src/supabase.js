7a86c44f10cb03bf8d60910e5dc80389a9f0495c
catherinemino <catherine.mino@marlborough.org>
2026-03-23 10:15:58 -0700

    hardcode supabase credentials

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// 👇 Paste your values from Supabase → Project Settings → API
-const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const SUPABASE_URL = 'https://kasamrxtbvebnwxriila.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_HXAzbq8K0wf2nIP0U_L6VA_q_XvGaFx';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
