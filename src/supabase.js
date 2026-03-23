d3b13708c0c42f75539230b7805a39b333328d22
catherinemino <catherine.mino@marlborough.org>
2026-03-23 10:21:28 -0700

    fix supabase key - use legacy JWT

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://kasamrxtbvebnwxriila.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_HXAzbq8K0wf2nIP0U_L6VA_q_XvGaFx';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imthc2Ftcnh0YnZlYm53eHJpaWxhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4MzA1NTYsImV4cCI6MjA4ODQwNjU1Nn0.98Tx4r3IK8blhTc6N2BLQ-xlaSkrcvfV3v2dQBXKW5c';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
